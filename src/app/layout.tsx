5f20abac536c39893ddbf85915bb87b4d68864fb
BHALALA MEETKUMAR RAMESHBHAI <24bce042@nirmauni.ac.in>
2026-06-26 16:08:29 +0530

    feat: implement user dashboard with message handling and profile link sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Toaster } from "@/components/ui/sonner"
 import AuthProvider from "@/context/AuthProvider";
+import './globals.css'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,13 +27,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <AuthProvider>
           {children}
-        </body>
-      </AuthProvider>
+          <Toaster/>
+        </AuthProvider>
+      </body>
     </html>
   );
 }
